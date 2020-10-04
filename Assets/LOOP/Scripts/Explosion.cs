790f38ef6707de1dae6f90cc1289ab4796ccafa6
Jan Stempkowski <the.jonu@gmail.com>
2020-10-04 17:15:52 +0000

    Physics layers and tags, mines, smoke

---
+++ Assets/LOOP/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public float lifeTime;
+
+    private float _timer;
+
+    void Start()
+    {
+        _timer = 0;
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if(_timer > lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/LOOP/Scripts/Tools/Explosive.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Tool tool;
+
+    public float explosionForce;
+    public float explosionRange;
+    public float upwardsMod;
+
+    public List<string> triggers;
+
+    public GameObject explosionPrefab;
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject col = collision.gameObject;
+
+        // Check for trigger
+        bool triggered = false;
+        foreach(string trigger in triggers)
+        {
+            if (col.CompareTag(trigger))
+            {
+                triggered = true;
+            }
+        }
+
+        // Explode on collision
+        if (triggered)
+        {
+            PhysicsManager.Instance.CreateExplosion(rb.position, explosionForce, explosionRange, upwardsMod);
+            Instantiate(explosionPrefab, rb.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
+    }
+
+}

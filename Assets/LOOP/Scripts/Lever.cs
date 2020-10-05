fd18997d57eaedc9a07b66f7851cfff933667af2
Jan Stempkowski <the.jonu@gmail.com>
2020-10-05 11:41:24 +0000

    Slight folder restructure, lever script

---
+++ Assets/LOOP/Scripts/Lever.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Transform stick;
+    public bool startingOn;
+
+    public float turnAngle;
+
+    public EventHandler LeverOff;
+    public EventHandler LeverOn;
+
+    private bool _on;
+
+
+
+    void Start()
+    {
+
+        float startAngle;
+        if (startingOn)
+        {
+            _on = true;
+            startAngle = turnAngle;
+        }
+        else
+        {
+            _on = false;
+            startAngle = -turnAngle;
+        }
+        stick.rotation = Quaternion.Euler(new Vector3(0, 0, startAngle));
+    }
+
+
+    void Update()
+    {
+        float stickAngle = stick.rotation.eulerAngles.z;
+
+        if(!_on && stickAngle > turnAngle)
+        {
+            // Turn on
+
+            _on = true;
+            LeverOn?.Invoke(this, new EventArgs());
+        }
+
+        else if(_on && stickAngle < -turnAngle)
+        {
+            // Turn off
+
+            _on = false;
+            LeverOff?.Invoke(this, new EventArgs());
+        }
+    }
+}

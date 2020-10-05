916454d84918e697160f45bfeee9b3b55ffe8553
Jan Stempkowski <the.jonu@gmail.com>
2020-10-05 18:34:30 +0000

    Sticky grenades

---
+++ Assets/LOOP/Scripts/StickyBomb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyBomb : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject col = collision.gameObject;
+
+        if (col.CompareTag("Terrain"))
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector2.zero;
+            rb.rotation = 0;
+        }
+    }
+}
